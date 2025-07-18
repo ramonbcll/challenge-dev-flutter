33207e8a518a07a6db5e0c82680fa8185853913d
Ramon Basto <rbcll@ic.ufal.br>
2025-07-17 21:57:30 -0300

    feat: add RegisterController to handle student registration

---
+++ application/lib/src/provider/register_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:application/src/models/student.dart';
+import 'package:application/src/repositories/student_repository.dart';
+
+class RegisterController extends ChangeNotifier {
+  final StudentRepository _studentRepository;
+
+  RegisterController(this._studentRepository);
+
+  Future<Map<String, dynamic>> addStudent(Student student) async {
+    final result = await _studentRepository.addStudent(student);
+    notifyListeners();
+    return result;
+  }
+}
